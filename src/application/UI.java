9872820c0f23f83f3852d1178a1735ed3a6312ce
Faitly <fty.57@alu.ufc.br>
2019-12-28 11:14:56 -0300

    Handling exceptions

--- src/application/UI.java
+++ src/application/UI.java
@@ -8,7 +8,10 @@ import chess.ChessPosition;
 
 public class UI {
 
-	
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
 	
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
